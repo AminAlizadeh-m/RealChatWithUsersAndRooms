413fd5a20ba6d649cc21a2d73886f890f195ee43
Amin <aminalizadeh.developer@gmail.com>
2021-04-12 19:16:04 +0430

    Socket.io added to project

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 require('dotenv').config();
-
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => console.log(
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when client connects
+io.on('connection', socket => {
+    // Emit to user when connects (Just send to user that connected)
+    socket.emit('message', 'Welcome to chat!');
+
+    // Brodcast when a user connects (send to all user exept that user connected)
+    socket.broadcast.emit('message', 'A user has joined the chat!');
+
+    // Brodcast when a user disconencts from chat
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    });
+});
+
+server.listen(PORT, () => console.log(
     `http://localhost:${PORT}`
 ));
